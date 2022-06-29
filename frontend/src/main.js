37d79c028fd9419bdefab49a7dd5722f01f5d71f
Silas Jimmy <jimmysilas17@gmail.com>
2022-06-29 09:41:23 +0300

    refactor: Add pinia plugin to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import './registerServiceWorker'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  pinia,
   render: h => h(App)
 }).$mount('#app')
