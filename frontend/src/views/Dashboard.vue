f631ffa4bce7459774234f55469362031f86b747
Silas Jimmy <jimmysilas17@gmail.com>
2022-06-29 13:49:51 +0300

    feat: Create dashboard view

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card tile flat height="100%">
+    <v-card-title>Dashboard</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  title: "Dashboard",
+};
+</script>
